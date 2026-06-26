28047e43f23b7ef94b3743dea36c1e20a5bcd34d
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2026-06-26 17:35:48 -0500

    Updated launcher to use the pom.xml

--- launcher.py
+++ launcher.py
@@ -1,6 +1,9 @@
 import subprocess
 import os
-programs = {1:["python","employee_app/employee_terminal.py"],2:["java", "manager_app/manager_terminal.java"]}
+programs = {
+    1: ["python", "employee_app/employee_terminal.py"],
+    2: ["mvn", "-q", "-f", "manager_app/pom.xml", "exec:java"],
+}
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -9,11 +12,8 @@ while True:
         choice = int(choice)
         if choice == 1 or choice == 2:
             subprocess.run(["python", "employee_app/db/seed.py"])
-            language = programs[choice][0]
-            location = programs[choice][1]
-
             os.system('cls' if os.name == 'nt' else 'clear')
-            subprocess.run([language, location])
+            subprocess.run(programs[choice])
         else:
             print("\nInvalid input. Please enter a valid number.")
 
